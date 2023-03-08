b606bc0b7528d141a4fa5814bdc82e1cf7b5e8b9
fadlyian <mansyuraw24@gmail.com>
2023-03-08 10:18:50 +0700

    membuat Todolist Service

---
+++ tests/Feature/TodolistServiceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\TodolistService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodolistServiceTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    // public function test_example(): void
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+    public TodolistService $todolistService;
+
+    public function setUp():void
+    {
+        parent::setUp();
+
+        $this->todolistService = $this->app->make(TodolistService::class);
+    }
+
+    public function testTodolistNotNull()
+    {
+        self::assertNotNull($this->todolistService);
+    }
+}
