15aa012365f668703b5011632daa3060f9e9c4bd
fadlyian <mansyuraw24@gmail.com>
2023-03-11 09:35:22 +0700

    membuat controller todolist dan membuat halaman todo

---
+++ tests/Feature/TodolistControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodolistControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+   
+     public function testTodolist()
+     {
+        $this->withSession([
+            "user" => "khannedy",
+            "todolist" => [
+                [
+                    "id" => "1",
+                    "todo" => "Eko",
+                ],
+                [
+                    "id" => "2",
+                    "todo" => "Kurniawan",
+                ],
+            ],
+        ])->get('/todolist')->assertSeeText("1")
+                            ->assertSeeText("Eko")
+                            ->assertSeeText("2")
+                            ->assertSeeText("Kurniawan");
+     }
+}
